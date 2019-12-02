5643bef8b02ab68506a0fe1336d36e69031841dd
AndreiRoibu <aroibu1@gmail.com>
2019-12-02 22:10:39 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Basic Numpy',
+    version='0.0.1',
+    description='A series of basic Numpy files',
+    license='BSD 3-clause license',
+    maintainer='AndreiRoibu',
+    maintainer_email='aroibu1@gmail.com',
+    install_requires=[
+        'numpy'
+    ],
+)
